f89234a5dffa67a87a68a87136fefeb3a92feb09
Praveen Chaudhary <pchaudhary@linkedin.com>
2020-03-21 22:00:55 -0700

    [config_mgmt.py]: Redirect logs to syslog. (#19)
    * [config_mgmt.py]: Redirect logs to syslog.
    
    Signed-off-by: Praveen Chaudhary pchaudhary@linkedin.com
    
    * [config_mgmt.py]: Call openlog and closelog one time.
    
    Changes:
    1.) Call openlog and closelog one time.
    2.) call syslog directly.
    3.) mask loglevel based on debug argument to class.
    4.) Class destructor to call closelog.
    
    Signed-off-by: Praveen Chaudhary pchaudhary@linkedin.com
    
    * [config_mgmt.py]: Fixing minor typo.
    
    Signed-off-by: Praveen Chaudhary pchaudhary@linkedin.com
    
    * [config_mgmt.py]: Call openlog and closelog each time from a wrapper function.
    
    It is mandotary to call openlog and closelog eachtime while using syslog library.
    Since it is one class Instance shared among all python applications.
    
    Signed-off-by: Praveen Chaudhary pchaudhary@linkedin.com

--- config/config_mgmt.py
+++ config/config_mgmt.py
@@ -19,6 +19,7 @@ try:
     from time import sleep as tsleep
 
     import sonic_yang
+    import syslog
     import re
 
 except ImportError as e:
@@ -45,23 +46,19 @@ class configMgmt():
             self.allowExtraTables = allowExtraTables
             self.oidKey = 'ASIC_STATE:SAI_OBJECT_TYPE_PORT:oid:0x'
 
-            self.DEBUG_FILE = None
-            if debug:
-                self.DEBUG_FILE = '_debug_config_mgmt'
-                with open(self.DEBUG_FILE, 'a') as df:
-                    df.write('--- Start config_mgmt logging ---\n\n')
+            # logging vars
+            self.SYSLOG_IDENTIFIER = "configMgmt"
+            self.DEBUG = debug
 
             self.sy = sonic_yang.sonic_yang(YANG_DIR, debug=debug)
             # load yang models
             self.sy.loadYangModel()
-
             # load jIn from config DB or from config DB json file.
             if source.lower() == 'configdb':
                 self.readConfigDB()
             # treat any other source as file input
             else:
                 self.readConfigDBJson(source)
-
             # this will crop config, xlate and load.
             self.sy.load_data(self.configdbJsonIn, self.allowExtraTables)
 
@@ -71,18 +68,17 @@ class configMgmt():
 
         return
 
-    def logInFile(self, header="", obj=None, json=False):
+    def __del__(self):
+        pass
 
-        if self.DEBUG_FILE:
-            with open(self.DEBUG_FILE, 'a') as df:
-                time = datetime.now()
-                df.write('\n\n{}: {}\n'.format(time, header))
-                if json:
-                    dump(obj, df, indent=4)
-                elif obj:
-                    #print(obj)
-                    df.write('{}: {}'.format(time, obj))
-                df.write('\n----')
+    def sysLog(self, debug=syslog.LOG_INFO, msg=None):
+
+        # log debug only if enabled
+        if self.DEBUG == False and debug == syslog.LOG_DEBUG:
+            return
+        syslog.openlog(self.SYSLOG_IDENTIFIER)
+        syslog.syslog(debug, msg)
+        syslog.closelog()
 
         return
 
@@ -93,7 +89,7 @@ class configMgmt():
         #print(type(self.configdbJsonIn))
         if not self.configdbJsonIn:
             raise(Exception("Can not load config from config DB json file"))
-        self.logInFile('Reading Input', self.configdbJsonIn, True)
+        self.sysLog(msg='Reading Input {}'.format(self.configdbJsonIn))
 
         return
 
@@ -110,7 +106,8 @@ class configMgmt():
         configdb.connect()
         deep_update(data, FormatConverter.db_to_output(configdb.get_config()))
         self.configdbJsonIn =  FormatConverter.to_serialized(data)
-        #self.logInFile('Reading Input', self.configdbJsonIn, True)
+        self.sysLog(syslog.LOG_DEBUG, 'Reading Input from ConfigDB {}'.\
+            format(self.configdbJsonIn))
 
         return
 
@@ -124,7 +121,7 @@ class configMgmt():
         configdb.connect(False)
         deep_update(data, FormatConverter.to_deserialized(jDiff))
         data = sort_data(data)
-        self.logInFile("Write in DB: Last Data\n", data)
+        self.sysLog(msg="Write in DB: {}".format(data))
         configdb.mod_config(FormatConverter.output_to_db(data))
 
         return
@@ -134,7 +131,7 @@ class configMgmt():
     """
     def checkKeyinAsicDB(self, key, db):
 
-        self.logInFile('Check Key in Asic DB: {}'.format(key))
+        self.sysLog(msg='Check Key in Asic DB: {}'.format(key))
         try:
             # chk key in ASIC DB
             if db.exists('ASIC_DB', key):
@@ -147,9 +144,9 @@ class configMgmt():
 
     def testRedisCli(self, key):
         # To Debug
-        if self.DEBUG_FILE:
+        if self.DEBUG:
             cmd = 'sudo redis-cli -n 1 hgetall "{}"'.format(key)
-            self.logInFile("Running {}".format(cmd))
+            self.sysLog(syslog.LOG_DEBUG, "Running {}".format(cmd))
             print(cmd)
             system(cmd)
         return
@@ -186,11 +183,11 @@ class configMgmt():
     def verifyAsicDB(self, db, ports, portMap, timeout):
 
         print("Verify Port Deletion from Asic DB, Wait...")
-        self.logInFile("Verify Port Deletion from Asic DB, Wait...")
+        self.sysLog(msg="Verify Port Deletion from Asic DB, Wait...")
 
         try:
             for waitTime in range(timeout):
-                self.logInFile('Check Asic DB: {} try'.format(waitTime+1))
+                self.sysLog(msg='Check Asic DB: {} try'.format(waitTime+1))
                 # checkNoPortsInAsicDb will return True if all ports are not
                 # present in ASIC DB
                 if self.checkNoPortsInAsicDb(db, ports, portMap):
@@ -201,8 +198,8 @@ class configMgmt():
             if waitTime + 1 == timeout:
                 print("!!!  Critical Failure, Ports are not Deleted from \
                     ASIC DB, Bail Out  !!!")
-                self.logInFile("!!!  Critical Failure, Ports are not Deleted from \
-                    ASIC DB, Bail Out  !!!")
+                self.sysLog(syslog.LOG_CRIT, "!!!  Critical Failure, Ports \
+                    are not Deleted from ASIC DB, Bail Out  !!!")
                 raise(Exception("Ports are present in ASIC DB after timeout"))
 
         except Exception as e:
@@ -233,7 +230,7 @@ class configMgmt():
             # Save Port OIDs Mapping Before Deleting Port
             dataBase = SonicV2Connector(host="127.0.0.1")
             if_name_map, if_oid_map = port_util.get_interface_oid_map(dataBase)
-            self.logInFile('if_name_map', obj=if_name_map, json=True)
+            self.sysLog(syslog.LOG_DEBUG, 'if_name_map {}'.format(if_name_map))
 
             # If we are here, then get ready to update the Config DB, Update
             # deletion of Config first, then verify in Asic DB for port deletion,
@@ -263,7 +260,7 @@ class configMgmt():
 
         configToLoad = None; deps = None
         try:
-            self.logInFile("delPorts ports:{} force:{}".format(ports, force))
+            self.sysLog(msg="delPorts ports:{} force:{}".format(ports, force))
 
             print('\nStart Port Deletion')
             deps = list()
@@ -272,13 +269,11 @@ class configMgmt():
             for port in ports:
                 xPathPort = self.sy.findXpathPortLeaf(port)
                 print('Find dependecies for port {}'.format(port))
-                self.logInFile('Find dependecies for port {}'.format(port))
+                self.sysLog(msg='Find dependecies for port {}'.format(port))
                 # print("Generated Xpath:" + xPathPort)
                 dep = self.sy.find_data_dependencies(str(xPathPort))
                 if dep:
                     deps.extend(dep)
-            self.logInFile('Dependencies', deps)
-
 
             # No further action with no force and deps exist
             if force == False and deps:
@@ -288,7 +283,7 @@ class configMgmt():
             # of deps fails, return immediately
             elif deps and force:
                 for dep in deps:
-                    self.logInFile('Deleting', dep)
+                    self.sysLog(msg='Deleting {}'.format(dep))
                     self.sy.delete_node(str(dep))
             # mark deps as None now,
             deps = None
@@ -297,7 +292,7 @@ class configMgmt():
             for port in ports:
                 xPathPort = self.sy.findXpathPort(port)
                 print("Deleting Port: " + port)
-                self.logInFile('Deleting Port:{}'.format(port), xPathPort)
+                self.sysLog(msg='Deleting Port:{}'.format(port))
                 self.sy.delete_node(str(xPathPort))
 
             # Let`s Validate the tree now
@@ -330,18 +325,17 @@ class configMgmt():
 
         configToLoad = None
         try:
-            self.logInFile('\nStart Port Addition')
-            self.logInFile("addPorts ports:{} loadDefConfig:{}".format(ports, loadDefConfig))
-            self.logInFile("addPorts Args portjson: ", portJson)
+            self.sysLog(msg='Start Port Addition')
+            self.sysLog(msg="addPorts ports:{} loadDefConfig:{}".\
+                format(ports, loadDefConfig))
+            self.sysLog(msg="addPorts Args portjson {}".format(portJson))
 
             print('\nStart Port Addition')
             # get default config if forced
             defConfig = dict()
             if loadDefConfig:
                 defConfig = self.getDefaultConfig(ports)
-                self.logInFile('Default Config for {}'.format(ports), \
-                    defConfig, json=True)
-            #prtprint(defConfig)
+                self.sysLog(msg='Default Config: {}'.format(defConfig))
 
             # get the latest Data Tree, save this in input config, since this
             # is our starting point now
@@ -357,7 +351,7 @@ class configMgmt():
             # We do not allow new table merge while adding default config.
             if loadDefConfig:
                 print("Merge Default Config for {}".format(ports))
-                self.logInFile("Merge Default Config for {}".format(ports))
+                self.sysLog(msg="Merge Default Config for {}".format(ports))
                 self.mergeConfigs(self.configdbJsonOut, defConfig, True)
 
             # create a tree with merged config and validate, if validation is
@@ -384,11 +378,11 @@ class configMgmt():
         try:
             self.sy.validate_data_tree()
         except Exception as e:
-            self.logInFile('Data Validation Failed')
+            self.sysLog(msg='Data Validation Failed')
             return False
 
         print('Data Validation successful')
-        self.logInFile('Data Validation successful')
+        self.sysLog(msg='Data Validation successful')
         return True
 
     """
@@ -511,7 +505,6 @@ class configMgmt():
             # Process diff and create Config which can be updated in Config DB
             configToLoad = self.createConfigToLoad(configDBdiff, \
                 self.configdbJsonIn, self.configdbJsonOut)
-            self.logInFile("Config Diff to Load: {}", configToLoad, True)
 
         except Exception as e:
             print("Update to Config DB Failed")
@@ -757,56 +750,23 @@ def testRun_Delete_Add_Port(cmode, nmode, loadDef):
     }
 
     # TODO: Verify config in Config DB  after writing to config DB.
-    print('Test Run Delete Ports')
+    print('Test Run Break Out Ports')
     try:
         cm = configMgmt('configDB', debug=True)
+
+        delPorts = delPortDict[cmode]
+        addPorts = delPortDict[nmode]
+        portJson = portJsonDict[nmode].copy()
+
+        cm.breakOutPort(delPorts=delPorts, addPorts= addPorts, portJson=portJson,\
+            force=True, loadDefConfig=loadDef)
+
+        print("\n***Port Break Out Test Passed***\n")
+
     except Exception as e:
         print(e)
+        print("\n***Port Break Out Test Failed***\n")
         return
-
-    delPorts = delPortDict[cmode]
-
-    deps, ret = cm.deletePorts(delPorts=delPorts, force=True)
-    if ret == False:
-        print("Port Deletion Test failed")
-        return None
-
-    print("Verify Port Table in config DB for Deletion")
-    portTable = getPortConfigDB()
-    for port in delPorts:
-        if portTable.get(port):
-            print("Port {} is not deleted from config DB".format(port))
-            print("Port Deletion Test Failed")
-            return
-
-    print("\n***Port Deletion Test Passed***\n")
-
-    s = 5
-    print("Wait for {} Secs".format(s))
-    from time import sleep
-    sleep(s)
-
-    ports = delPortDict[nmode]
-    portJson = portJsonDict[nmode].copy()
-
-    ret = cm.addPorts(ports=ports, portJson=portJson, loadDefConfig=loadDef)
-    if ret == False:
-        print("Port Addition Test failed")
-        return None
-
-    # get the port list back, AddPorts may have changed them
-    print("Verify Port Table in config DB for Addition")
-    ports = delPortDict[nmode]
-    portJson = portJsonDict[nmode]
-    portTable = getPortConfigDB()
-    #print(portTable)
-    for port in ports:
-        if portJson['PORT'][port] != portTable[port]:
-            print("Port {} is not added to config DB correctly".format(port))
-            print("Port Deletion Test Failed")
-            return
-
-    print("\n***Port Addition Test Passed***\n")
 
     return
 
